1a54880d9d1f126a26a6b0710d98a3064a201b43
Oleksii Bondar <v-obondar@expedia.com>
2016-07-30 07:20:50 -0700

    Rename function

--- contentFiller.js
+++ contentFiller.js
@@ -10,7 +10,7 @@
 // @require      https://rawgit.com/notifyjs/notifyjs/master/dist/notify.js
 
 // ==/UserScript==
-GM_addStyle(".notifyjs-foo-base { opacity: 0.85; width: 400px; background: #EBF21B; padding: 5px; border-radius: 10px; color: white; link: white; vlink: white; alink: white; } .notifyjs-foo-base .div {  float: left; margin: 10px 0 0 10px; text-align: right; color: white; }");
+GM_addStyle(".notifyjs-foo-base { opacity: 0.85; width: 400px; background: #FFCB00; padding: 5px; border-radius: 10px; color: white; link: white; vlink: white; alink: white; } .notifyjs-foo-base .div {  float: left; margin: 10px 0 0 10px; text-align: right; color: white; }");
 
 $.notify.addStyle('foo', {
   html: 
@@ -33,26 +33,26 @@ $.notify({
 });
 
 $(document).ready(function () {  
-    var departDate = parseDate(localStorage.getItem('departDate'));
-    var returnDate = parseDate(localStorage.getItem('returnDate'));
+    var departDate = convertDate(localStorage.getItem('departDate'));
+    var returnDate = convertDate(localStorage.getItem('returnDate'));
     
     var returnAirport = localStorage.getItem('returnAirport').split(' ')[0];
     
-    var restApiUrl = 'https://app.ticketmaster.com/discovery/v2/events.json?apikey=SLFZ5pYZ6UlXoUpRG5IJhtGeE4ohRpGs&city='+returnAirport+ '&countryCode=US&startDateTime='+departDate+'T00:00:00Z&endDateTime='+returnDate+'T00:00:00Z&size=10';
+    var restApiUrl = 'https://app.ticketmaster.com/discovery/v2/events.json?apikey=SLFZ5pYZ6UlXoUpRG5IJhtGeE4ohRpGs&city='+returnAirport+'&countryCode=US&startDateTime='+departDate+'T00:00:00Z&endDateTime='+returnDate+'T00:00:00Z&size=10';
     console.log(departDate + ' ' + returnDate + ' '+ returnAirport);
     console.log(restApiUrl);
     
     $.getJSON( restApiUrl, function(data) {
         var contentTable = '';
         $.each(data._embedded.events, function(index, event) {
-            contentTable += '<tr><td><a href="'+ event.url + '">' + event.name + '</a></td></tr>';
+            contentTable += '<tr><td><a href="'+event.url+'">'+event.name+'</a></td></tr>';
         });
         $("#results").append(contentTable);
         console.log(contentTable);
     });
 });
 
-function parseDate(inputDate) {
+function convertDate(inputDate) {
     var inputDateLocal = inputDate.replace('/', '-').replace('/', '-').replace('/', '-');
     var inputDateArr = inputDateLocal.split('-');
     
